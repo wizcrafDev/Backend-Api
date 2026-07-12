65a49cdb255aa0260f87a0e2239c85687ed7cfe3
Nicholas Chukwuma <nickchukws@gmail.com>
2026-07-12 16:11:30 +0100

    completed the CRUD operations

--- src/server.js
+++ src/server.js
@@ -16,11 +16,47 @@ app.get("/users", async function (req, res) {
   res.status(200).json({ Users });
 });
 
+//route to post users, route handler
 app.post("/users", (req, res) => {
-  Users.push(res.body);
+  Users.push(req.body);
   res.status(201).res.json(req.body);
 });
 
-//route to post users, route handler
+app.patch("/users", async (req, res) => {
+  try {
+    const newUser = await Users.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+app.put("/users", async (req, res) => {
+  try {
+    const newUser = await Users.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const deletedUser = await Users.findByIdAndDelete(req.params.id);
+
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
 
 app.listen(5000, () => console.log("server started"));
